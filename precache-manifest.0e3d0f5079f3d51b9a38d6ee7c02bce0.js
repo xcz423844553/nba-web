d2b35c129b3886c9ff15b27872621916db604d6a
Az <airz.xu@gmail.com>
2019-12-08 16:14:19 -0800

    Updates

---
+++ precache-manifest.0e3d0f5079f3d51b9a38d6ee7c02bce0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "554ccedc31e8c35399629c05f3effb99",
+    "url": "/nba-web/index.html"
+  },
+  {
+    "revision": "fe37040bf5a3b8aa9b67",
+    "url": "/nba-web/static/css/2.7f669fb7.chunk.css"
+  },
+  {
+    "revision": "3d2e4c8b83c5a878d524",
+    "url": "/nba-web/static/css/main.47c9deb0.chunk.css"
+  },
+  {
+    "revision": "fe37040bf5a3b8aa9b67",
+    "url": "/nba-web/static/js/2.4fa078ad.chunk.js"
+  },
+  {
+    "revision": "200adb52ba43a6717952f5f45e02d6cb",
+    "url": "/nba-web/static/js/2.4fa078ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d2e4c8b83c5a878d524",
+    "url": "/nba-web/static/js/main.53a66ee7.chunk.js"
+  },
+  {
+    "revision": "159cd37b15c6d2c06800",
+    "url": "/nba-web/static/js/runtime-main.71a9aa8f.js"
+  },
+  {
+    "revision": "63bc98a9252819c9a4d2bd207dec5f50",
+    "url": "/nba-web/static/media/nba-logoman-word-white.63bc98a9.svg"
+  }
+]);
